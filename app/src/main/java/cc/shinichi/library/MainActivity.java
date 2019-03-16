7230488e5705a1830b25c2b2327dc4ee365fdb6e
勾勾 <gougou@16fan.com>
2019-03-16 18:04:28 +0800

    🚧施工中...

---
+++ app/src/main/java/cc/shinichi/library/MainActivity.java
@@ -0,0 +1,12 @@
+package cc.shinichi.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ librarybanner/src/main/java/cc/shinichi/librarybanner/SBanner.java
@@ -0,0 +1,22 @@
+package cc.shinichi.librarybanner;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class SBanner extends RelativeLayout {
+
+    public SBanner(Context context) {
+        this(context, null);
+    }
+
+    public SBanner(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SBanner(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
